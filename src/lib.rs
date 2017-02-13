7d075c9df5dc7c7c1b5a9dd5ec4791cc0a03b58c
SiIky <siikyatgith@openaliasbox.org>
2017-02-13 17:52:43 +0000

    Generalize and simplify convertion process

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,7 @@
-use std::cmp;
-
-mod tests;
-pub mod dict;
-
-use dict::{Dict, KanaConvertionTable};
-
-pub fn to_hiragana(s: &String, d: &dict::Dict) -> String {
-    unimplemented!();
-}
-
-pub fn to_katagana(s: &String, d: &dict::Dict) -> String {
-    unimplemented!();
-}
-
-pub fn to_kana(s: &String, d: &dict::Dict) -> String {
-    let vec: Vec<String> = syllables(d, s);
-    let mut ret: String = String::new();
-    for syl in vec {
-        ret.push_str(&normalize(&syl));
-    }
-    ret
-}
-
-// converts everything as is according to the dict
-pub fn gen_convert(s: &String, d: &dict::Dict) -> String {
-    let vec: Vec<String> = syllables(d, s);
-    let mut ret: String = String::new();
-    for syl in vec {
-        ret.push_str(&convert_syllable(d, &syl));
-    }
-    ret
-}
-
 // do_work :: Dict -> String -> String
-// do_work = concat $ map convert_syllable $ syllables
-//
+// do_work d s = concat $ map (f d) (words s)
+//     where f = (\(d, w) -> concat $ map (convert_syllable d) (syllables w)
+
 // syllables :: Dict -> String -> [String]
 // syllables _ [] = []
 // syllables d s@(x:xs)
@@ -43,14 +10,60 @@ pub fn gen_convert(s: &String, d: &dict::Dict) -> String {
 //     where
 //          max = let len = length s in if (>) len 3 then 3 else len
 //       -- aux :: Int -> Dict -> String -> (Bool, (String, String))
-//          aux 0 _ _ = (false, ([], []))
-//          aux _ _ [] = (false, ([], []))
+//          aux 0 _ _ = (False, ([], []))
+//          aux _ _ [] = (False, ([], []))
 //          aux n d s
-//              | contains_key d (take n s) = (true, (take n s, drop n s)
-//              | otherwise = syllables (n-1) d s
+//              | contains_key d (take n s) = (True, (take n s, drop n s))
+//              | otherwise = aux (n-1) d s
 //          cond = aux max d s
 
-fn syllables(d: &dict::Dict, original: &String) -> Vec<String> {
+// to_hiragana :: Dict -> String -> String
+// to_hiragana d h = do_work d (to_lowercase h)
+
+// to_katakana :: Dict -> String -> String
+// to_katakana d k = do_work d (to_uppercase k)
+
+// to_kana :: Dict -> String -> String
+// to_kana = do_work
+
+use std::cmp;
+
+pub mod dict;
+
+use dict::Dict;
+
+pub fn to_hiragana(d: &Dict, s: &String) -> String {
+    let s = s.to_lowercase();
+    gen_convert(d, &s)
+}
+
+pub fn to_katakana(d: &Dict, s: &String) -> String {
+    let s = s.to_uppercase();
+    gen_convert(d, &s)
+}
+
+pub fn to_kana(d: &Dict, s: &String) -> String {
+    gen_convert(d, &s)
+}
+
+// Converts a String as is
+fn gen_convert(d: &Dict, s: &String) -> String {
+    let mut ret: String = String::new();
+
+    for w in s.split_whitespace() {
+        let w: String = normalize(&w.to_string());
+        let ss: Vec<String> = syllables(&d, &w)
+            .iter()
+            .map(|ref s| convert_syllable(d, &s))
+            .collect();
+        for syl in ss {
+            ret.push_str(syl.as_str());
+        }
+    }
+    ret
+}
+
+fn syllables(d: &Dict, original: &String) -> Vec<String> {
     if original.len() == 0 {
         return vec![];
     }
@@ -86,44 +99,27 @@ fn syllables(d: &dict::Dict, original: &String) -> Vec<String> {
     ret
 }
 
-fn is_syllable(d: &dict::Dict, k: &String) -> bool {
-    let u = k.clone();
-    let l = k.clone();
-    u.to_uppercase();
-    l.to_lowercase();
-    d.contains_key(&u) || d.contains_key(&l)
+// Given a String (expected to be normalized), returns `true` if it is a syllable and
+// `false` otherwise.
+fn is_syllable(d: &Dict, k: &String) -> bool {
+    d.contains_key(k)
 }
 
+// Searches for uppercase characters in a String and, if it finds any, turns the whole String to
+// uppercase.
 fn normalize(s: &String) -> String {
     match s.chars().fold(false, |acc, c| acc || c.is_uppercase()) {
         true => s.to_uppercase(),
-        false => s.clone(),
+        false => s.to_string(),
     }
 }
 
-fn convert_syllable(d: &dict::Dict, ow: &String) -> String {
-    match d.get(&ow.clone()) {
+// Converts a String (expected to be a syllable) to kana.
+pub fn convert_syllable(d: &Dict, ow: &String) -> String {
+    // <pingveno> Because String impls Borrow<str>
+    // `&ow.to_string()` => `ow`
+    match d.get(&normalize(&ow)) {
         Some(c) => c.clone(),
         None => ow.clone(),
     }
-}
-
-pub fn do_work(d: &dict::Dict, w: &String) -> String {
-    let mut ret = String::new();
-    let sylvec: Vec<String> = syllables(d, w)
-        .iter()
-        .map(|ref s| convert_syllable(d, &s))
-        .collect();
-
-    for s in sylvec {
-        ret.push_str(s.as_str());
-    }
-
-    ret
-}
-
-pub fn get_dict() -> dict::Dict {
-    let mut ret = Dict::new();
-    ret.init();
-    ret
 }
