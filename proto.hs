1cc72a10c9285cfec97be28857a6abca002ea5f6
SiIky <siiky@users.noreply.github.com>
2017-04-19 18:44:52 +0100

    Update and simplify `proto.hs`

--- proto.hs
+++ proto.hs
@@ -1,31 +1,37 @@
 data Dict
-data Type s = Auto s | Hiragana s | Katakana s
+data Type s = Auto s | Hira s | Kata s
+
+toKana :: Dict -> Type String -> String
+toKana d t = genConvert d $ unwrapType t
 
 genConvert :: Dict -> String -> String
-genConvert d s = concat $ map (convertWord d) (words s)
+genConvert d s = concatMap (convertWord d) (words s)
 
 convertWord :: Dict -> String -> String
-convertWord d s = concat $ map (convertSyllable d) (syllables d s)
+convertWord d s = concatMap (convertSyllable d) (syllables d s)
 
 syllables :: Dict -> String -> [String]
 syllables _ [] = []
 syllables d s@(x:xs)
-    | fst cond = (:) (fst $ snd $ cond) (syllables d (snd $ snd $ cond))
-    | otherwise = (:) [x] (syllables d xs)
+    | fst cond  = (:) (fst $ snd $ cond) (syllables d (snd $ snd $ cond))
+    | otherwise = (:) [x]                (syllables d xs)
     where
-        max = let len = length s in if (>) len 3 then 3 else len
+        max = let len = length s in
+                if (>) len 3
+                    then 3
+                    else len
         aux :: Int -> Dict -> String -> (Bool, (String, String))
         aux 0 _ _ = (False, ([], []))
         aux _ _ [] = (False, ([], []))
         aux n d s
             | d `containsKey` (take n s) = (True, (splitAt n s))
-            | otherwise = aux ((-) n 1) d s
+            | otherwise = aux (pred n) d s
         cond = aux max d s
 
-toKana :: Dict -> Type String -> String
-toKana d (Auto s) = genConvert d s
-toKana d (Hiragana s) = genConvert d $ toLowercse s
-toKana d (Katakana s) = genConvert d $ toUppercase s
+unwrapType :: Type String -> String
+unwrapType (Auto s) = s
+unwrapType (Hira s) = toLowercase s
+unwrapType (Kata s) = toUppercase s
 
 containsKey :: Dict -> String -> Bool
 containsKey = undefined
@@ -33,8 +39,8 @@ containsKey = undefined
 convertSyllable :: Dict -> String -> String
 convertSyllable = undefined
 
-toLowercse :: String -> String
-toLowercse = undefined
+toLowercase :: String -> String
+toLowercase = undefined
 
 toUppercase :: String -> String
 toUppercase = undefined
